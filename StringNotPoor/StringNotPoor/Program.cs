354a596c9c2659886c67877ed21a070f643cd242
Ramona Mihai <ramona.mihai87@yahoo.com>
2018-12-16 14:32:21 +0200

    Not that poor => good

---
+++ StringNotPoor/StringNotPoor/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace StringNotPoor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string input1 = "The lyrics is not that poor!";
+            Console.WriteLine($"{input1} => {NotPoor(input1)}.");
+            string input2 = "The lyrics is poor!";
+            Console.WriteLine($"{input2} => {NotPoor(input2)}.");
+        }
+
+        public static string NotPoor(string input)
+        {
+            if (input.Contains("not") && input.IndexOf("not") < input.IndexOf("poor"))
+            {
+                return input.Substring(0, input.IndexOf("not")) + "good" + input.Substring(input.IndexOf("poor") + 4);
+            }
+            return input;
+        }
+
+    }
+}
+
+
+
+
+
